4520a4c76aad1eb04c190ea4ab61224ab45b7bda
EclipseGarden <vivian.fryer02@gmail.com>
2023-03-03 09:57:00 -0500

    Updated work

---
+++ Firstjs.js
@@ -0,0 +1,7 @@
+let name='Vivian'
+let age= 21
+let city='Ottawa'
+
+console.log("Name:", name)
+console.log("Age:", age)
+console.log("City:", city)
---
+++ JS/jsproject.js
@@ -0,0 +1,13 @@
+let a=5;
+
+if (a<0) {
+    console.log("The number is negative")
+}
+
+else if (a=0) {
+    console.log("The number is zero")
+}
+
+else if (a>0) {
+    console.log("The number is positive")
+}
